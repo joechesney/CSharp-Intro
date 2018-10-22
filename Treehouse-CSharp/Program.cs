4e0558104dcf1ca298233fec36373694f74c23db
joechesney <33613119+joechesney@users.noreply.github.com>
2018-10-22 11:40:33 -0500

    successfully have a command and storing value as integer

--- Treehouse-CSharp/Program.cs
+++ Treehouse-CSharp/Program.cs
@@ -4,9 +4,24 @@ namespace Treehouse_CSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int runningTotal = 0;
+
+            // Prompt user for minutes exercised
+            Console.Write("Enter how many minutes you exercised: ");
+
+            string minutesExercised = Console.ReadLine();
+            int minutes = int.Parse(minutesExercised);
+            runningTotal = runningTotal + minutes;
+
+            // Add minutes exercised to total
+
+            // Display total minutes exercised to the screen
+            Console.WriteLine("You've exercised " + runningTotal + " minutes! Great job!");
+
+            // Repeat until user quits
+            // Main();
         }
     }
 }
